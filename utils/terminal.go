51710f348b0d5f1ebe2a9196559ea0ba8fd0b514
nireo <eemil.lehtikangas1@gmail.com>
2020-06-12 01:52:34 +0300

    made getting terminal dimensions easier by termui (removed unsafe)

--- utils/terminal.go
+++ utils/terminal.go
@@ -1,41 +1,17 @@
 package utils
 
 import (
-	"syscall"
-	"unsafe"
+	ui "github.com/gizak/termui/v3"
 )
 
-type winsize struct {
-	Row    uint16
-	Col    uint16
-	Xpixel uint16
-	Ypixel uint16
-}
-
 // GetTerminalWidth gets the terminals width so that we can adjust the ui elements accordingly
-func GetTerminalWidth() uint {
-	ws := &winsize{}
-	retCode, _, errno := syscall.Syscall(syscall.SYS_IOCTL,
-		uintptr(syscall.Stdin),
-		uintptr(syscall.TIOCGWINSZ),
-		uintptr(unsafe.Pointer(ws)))
-
-	if int(retCode) == -1 {
-		panic(errno)
-	}
-	return uint(ws.Col)
+func GetTerminalWidth() int {
+	terminalWidth, _ := ui.TerminalDimensions()
+	return terminalWidth
 }
 
 // GetTerminalHeight returns the height of the terminal
-func GetTerminalHeight() uint {
-	ws := &winsize{}
-	retCode, _, errno := syscall.Syscall(syscall.SYS_IOCTL,
-		uintptr(syscall.Stdin),
-		uintptr(syscall.TIOCGWINSZ),
-		uintptr(unsafe.Pointer(ws)))
-
-	if int(retCode) == -1 {
-		panic(errno)
-	}
-	return uint(ws.Row)
+func GetTerminalHeight() int {
+	_, terminalHeight := ui.TerminalDimensions()
+	return terminalHeight
 }
